d96040c226f38cb833f44fe96266d5c506908363
wilberC <wilber.carrascal@gmail.com>
2022-04-16 15:25:23 -0500

    add-post-model-and-table

--- application/models/postgres/base.py
+++ application/models/postgres/base.py
@@ -1,2 +1,3 @@
 from database import Base
 from application.models.postgres.user_model import User
+from application.models.postgres.post_model import Post
---
+++ application/models/postgres/post_model.py
@@ -0,0 +1,14 @@
+from database import Base
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import relationship
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(Text)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="post")
