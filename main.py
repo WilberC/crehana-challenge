e7408f68c42962a0a4d301c36e8b1d85584eade7
wilberC <wilber.carrascal@gmail.com>
2022-04-19 15:20:11 -0500

    add-routes

--- main.py
+++ main.py
@@ -3,12 +3,30 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from database import Base, engine
 from application.views.sync_data import sync_data_router, sync_all_data
+from application.views.user_view import user_router
+from application.views.post_view import post_router
+from application.views.comment_view import comment_router
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# === Routes ===
 app.include_router(sync_data_router)
+app.include_router(user_router)
+app.include_router(post_router)
+app.include_router(comment_router)
+# ===============
 
 templates = Jinja2Templates(directory="templates")
 
