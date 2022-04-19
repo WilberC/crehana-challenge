61aa84e6f40f9cd253a0caf9546ca822eb68aa50
wilberC <wilber.carrascal@gmail.com>
2022-04-19 04:16:47 -0500

    update-json-response-to-sync

--- main.py
+++ main.py
@@ -1,34 +1,22 @@
-from typing import Optional
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from database import SessionLocal, Base, engine
+from database import Base, engine
+from application.views.sync_data import sync_data_router, sync_all_data
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(sync_data_router)
 
 templates = Jinja2Templates(directory="templates")
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
 
 @app.get("/")
 async def read_item(request: Request):
     if request.headers.get('Content-Type') == 'application/json':
-        return {"item_id": 1}
+        return sync_all_data()
     else:
-        context = {"request": request, "asdf": 'a'}
+        context = {"request": request}
         return templates.TemplateResponse("home.html", context)
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
